583afe5a925029aaa3d7889887641102a0bcb92a
Kongkidakorn Chankana <60215215+kongggwp@users.noreply.github.com>
2022-10-29 12:48:20 +0700

    Create 0007.cpp

---
+++ Betaprogramming/00xx/0007.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    double r , pi = 3.141592653589793238462643383279502884197169399375105820974944 ;
+    cin >> r ;
+    printf("%.6f\n%.6f", pi*r*r , 2*r*r);
+}

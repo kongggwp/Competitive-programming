1a4d8281fb8d2b423252b3e3fed94738b9e463b1
Kongkidakorn Chankana <60215215+kongggwp@users.noreply.github.com>
2022-10-29 17:54:38 +0700

    Create 0015.cpp

---
+++ Betaprogramming/00xx/0015.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a , b ,c;
+    cin >> a >> b >> c;
+    if((b-a)>(c-b)){
+        cout << b-a-1 ;
+    }
+    else{
+        cout << c-b-1 ;
+    }
+}

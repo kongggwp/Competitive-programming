2e9676c6180c026a933465bc38563e9d7da090d6
Kongkidakorn Chankana <60215215+kongggwp@users.noreply.github.com>
2022-10-29 17:56:28 +0700

    Create 0017.cpp

---
+++ Betaprogramming/00xx/0017.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a[4];
+    for(int i=0 ; i<4 ;i++){
+        cin >> a[i];
+    }
+    sort(a , a+4);
+    cout << a[0] * a[2]; 
+}

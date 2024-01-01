5e25dbfadb69194785c9d354b0d3632ba159a72a
Kongkidakorn Chankana <60215215+kongggwp@users.noreply.github.com>
2024-01-01 17:09:35 +0700

    Update toi13_robot.cpp

--- Olympiad/TOI/toi13_robot.cpp
+++ Olympiad/TOI/toi13_robot.cpp
@@ -73,3 +73,41 @@ int main()
 	}
 	cout << cnt << ' ' << path;
 }
+
+/*เขียนเเบบไม่มึน
+#include<bits/stdc++.h>
+using namespace std;
+int n , m , vis[2005][2005] , cnt , all;
+int dx[]{1,0,-1,0} , dy[]{0,1,0,-1};
+char grid[2005][2005];
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	cin >> n >> m;
+	queue<pair<int,pair<int,int>>>q;
+	for(int i=0 ; i<n ; i++)
+	{
+		for(int j=0;  j<m ; j++)
+		{
+			cin >> grid[i][j];
+			if(grid[i][j] == 'X')q.push({0 , {i , j}}) , vis[i][j] = 1;
+			if(grid[i][j] == 'W')vis[i][j] = 1;
+		}
+	}
+	while(q.size())
+	{
+		int x = q.front().second.first , y = q.front().second.second , step = q.front().first;
+		q.pop();
+		for(int k=0 ; k<4 ; k++)
+		{
+			int xx = x + dx[k] , yy = y + dy[k];
+			if(xx<0 || yy<0 || xx>=n || yy>=m)continue;
+			if(vis[xx][yy])continue;
+			vis[xx][yy] = 1;
+			if(grid[xx][yy]=='A')all+=step+1 , cnt++;
+			q.push({step+1 , {xx , yy}});
+		}
+	}
+	cout << cnt << ' ' << all*2;
+}
+*/
